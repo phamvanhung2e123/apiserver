26ba13624027b46b11cf013d9108c01beb14600a
ファム ヴァンフン <phamvanhung2e123@gmail.com>
2020-11-14 21:26:58 +0900

    update db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+trip_names = [  "Romantic Spots for Honeymoons",
+                "Top Island Getaways",
+                "Most Romantic Destinations",
+                "Top Spots for Skiing" ]
+trip_names.each do |trip_name|
+  Event.create(name: trip_name, location: "tokyo", start_time: Time.now - 30.day, end_time: Time.now + 30.day)
+end
