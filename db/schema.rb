8009dde72ea2a2f12f03c56284df62012598e7e7
ファム ヴァンフン <phamvanhung2e123@gmail.com>
2020-11-14 22:46:13 +0900

    add partifipant

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_14_123722) do
+ActiveRecord::Schema.define(version: 2020_11_14_134554) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,24 @@ ActiveRecord::Schema.define(version: 2020_11_14_123722) do
     t.datetime "end_time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "participants", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "event_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_participants_on_event_id"
+    t.index ["user_id"], name: "index_participants_on_user_id"
+  end
+
+  create_table "participations", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "event_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_participations_on_event_id"
+    t.index ["user_id"], name: "index_participations_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +51,8 @@ ActiveRecord::Schema.define(version: 2020_11_14_123722) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "participants", "events"
+  add_foreign_key "participants", "users"
+  add_foreign_key "participations", "events"
+  add_foreign_key "participations", "users"
 end
