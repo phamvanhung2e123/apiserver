276c1ba4c18ee09f461fe7a9d5a9cf39594ceb44
ファム ヴァンフン <phamvanhung2e123@gmail.com>
2020-11-16 05:37:52 +0900

    update participants

--- spec/requests/participants_spec.rb
+++ spec/requests/participants_spec.rb
@@ -12,11 +12,27 @@ RSpec.describe 'Participants API', type: :request do
       expect(response).to have_http_status(:unauthorized)
     end
 
-    it 'returns status code unauthorized' do
-      jwt = JsonWebToken.encode(user_id: user.id)
-      get '/api/v1/events/'+event_id.to_s+'/participants', headers: { "Authorization" => "Bearer #{jwt}" }
-      expect(JSON.parse(response.body)["results"]).to be_empty
-      expect(response).to have_http_status(200)
+    context "when empty list" do
+      it 'returns status code 20' do
+        jwt = JsonWebToken.encode(user_id: user.id)
+        get '/api/v1/events/'+event_id.to_s+'/participants', headers: { "Authorization" => "Bearer #{jwt}" }
+        expect(JSON.parse(response.body)["results"]).to be_empty
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "when have list" do
+      it 'returns status code 20 and list user' do
+        jwt = JsonWebToken.encode(user_id: user.id)
+        (1..5).each { |i|
+          fake_user = FactoryBot.create(:user)
+          FactoryBot.create(:participant, event_id: event_id, user_id: fake_user.id)
+        }
+        get '/api/v1/events/'+event_id.to_s+'/participants', headers: { "Authorization" => "Bearer #{jwt}" }
+        expect(JSON.parse(response.body)["results"]).not_to be_empty
+        expect(JSON.parse(response.body)["results"].size).to eq(5)
+        expect(response).to have_http_status(:ok)
+      end
     end
   end
 end
