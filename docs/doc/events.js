434433e2f6c89c94e79e4f036603c6a26b92a875
ファム ヴァンフン <phamvanhung2e123@gmail.com>
2020-11-16 22:57:19 +0900

    update docs

--- docs/doc/events.js
+++ docs/doc/events.js
@@ -1,24 +1,4 @@
 /**
- * @api {get} /api/v1/authenticate Request auth_token
- * @apiName GetUser
- * @apiGroup User
- *
- * @apiParam {email} email Users email.
- * @apiParam {password} password Users password.
- *
- * @apiSuccess {String} auth_token Api key of the User.
- * @apiExample {bash} Curl example
- * curl -X POST http://localhost:3000/api/v1/authenticate \
- * -H 'content-type: multipart/form-data' \
- * -F 'email=john.doe@example.com' -F 'password=test123'
- * @apiSuccessExample Success-Response:
- *     HTTP/1.1 200 OK
- *     {
- *       "auth_token": "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHBpcnkiOjE2MDU1NjgxNzl9.MOirZDwT_UtTpZBFufmISlEcc8FViIYNnR-3GSvp4BM"
- *     }
- */
-
-/**
  * @api {get} /api/v1/events Request for list of events
  * @apiName GetEvents
  * @apiGroup Event
@@ -49,5 +29,3 @@
  *      ]
  *     }
  */
-
-https://github.com/apidoc/apidoc/blob/master/example/example.js
