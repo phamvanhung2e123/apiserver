347c5a24e71dce14cc357b7373dee8dd9df3010d
ファム ヴァンフン <phamvanhung2e123@gmail.com>
2020-11-19 16:13:37 +0900

    fix

--- docs/doc/users.js
+++ docs/doc/users.js
@@ -7,9 +7,11 @@
  * @apiSuccess {String} email
  * @apiSuccess {Date} created_at
  * @apiSuccess {Date} updated_at
- *
+ * @apiHeader {String} Authorization The token can be generated from authenticate api
+ * @apiHeaderExample {Header} Header-Example
+ *     "Authorization: Bearer TOKEN"
  * @apiExample {bash} Curl example
- * curl http://localhost:3000/api/v1/users
+ * curl http://localhost:3000/api/v1/users -H 'Authorization: Bearer TOKEN'
  * @apiSuccessExample Success-Response:
  *     HTTP/1.1 200 OK
  *  {
